48e0a7de00df89447e438255d02b4d20be64609b
jahanashirin09 <jahanashrn411@gmail.com>
2024-07-10 20:56:45 +0530

    three

--- index.js
+++ index.js
@@ -1,96 +1,139 @@
-const displayvalue =document.getElementById("displayvalue");
-const numberKeys = document.querySelectorAll('.number');
-const operatorKeys = document.querySelectorAll('.operator');
-const clearButton = document.getElementById('clear');
-const equalButton = document.getElementById('equal');
-let num1,num2,operator,display,operation;
-let operatorpressed=false;
-let result=0;
+const topScreen = document.querySelector("#screen-top");
+const bottomScreen = document.querySelector("#screen-bottom");
+let lastInput = [];
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+let result = 0;
+let summed = false;
 
 
-function add(a,b){
-    return a+b;
-}
-function subtract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
-function remainder(a,b){
-    return a%b;
-}
-function operate(num1,num2,operator){
-    num1=Number(num1);
-    num2=Number(num2);
-    switch(operator){
-        case 'add':
-          add(num1, num2);
-          break;
-        case 'subtract':
-          subtract(num1, num2);
-          break;
-        case 'multiply':
-          multiply(num1, num2);
-          break;
-        case 'divide':
-          divide(num1, num2);
-          break;
-        case'reminder':
-           remainder(num1,num2);
-          break;
-        default:
-          init()
 
-      }
-    
-}
-function init(){
-    num1=0;
-    num2=0;
-    result=0
-    display=''
-    displayvalue.textContent=result;
-}
-function updatedisplay(e){
-    display+=e.target.textContent;
-    displayvalue.innerHTML=display;
-    if(operatorpressed){
-        num2=e.target.textContent;
-        display=operate(operator,num1,num2);
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+};
+
+
+const operate = function () {
+    num2 = Number(lastInput.join("").toString());
+    console.log(num1);
+    console.log(num2);
+    if (operator === "") return;
+    if (operator === "+") result = add(num1, num2);
+    if (operator === "-") result = subtract(num1, num2);
+    if (operator === "x") result = multiply(num1, num2);
+    if (operator === "/") result = divide(num1, num2);
+    topScreen.textContent = `${checkIfFloat(num1)} ${operator} ${checkIfFloat(num2)} =`;
+    bottomScreen.textContent = checkIfFloat(result);
+    num1 = result
+    summed = true;
+};
+
+const display = function (btn) {
+    if (bottomScreen.textContent === "0") num2 = 0; //clear num2 if prev equation resulted in 0;
+
+    if (result) {
+        lastInput = [];
+        result = "";
     }
-    
+    if (lastInput.includes(".") && this.textContent === ".") return;
+    if (lastInput.length === 8) {
+        lastInput.length = 8;
+        return;
+    } else
+        lastInput.push(this.textContent);
+    bottomScreen.textContent = lastInput.join("").toString();
 }
-function handleoperation(e){
-    num1=display;
-    display+=e.target.innerHTML;
-    operation=e.target.datset.action;
-    isOperatorPressed=true;
-    if(result){
-        num1=result;
-    }
 
+const chainOperators = function (btn) {
+    num2 = Number(bottomScreen.textContent);
+    if (operator === "+") result = add(num1, num2);
+    if (operator === "-") result = subtract(num1, num2);
+    if (operator === "x") result = multiply(num1, num2);
+    if (operator === "/") result = divide(num1, num2);
+    num1 = checkIfFloat(result);
+    num2 = 0;
+    lastInput = [];
 }
-numberKeys.forEach(numberKey =>
-    numberKey.addEventListener('click', e => updateDisplay(e))
-  );
-  
-  operatorKeys.forEach(operator =>
-    operator.addEventListener('click', e => handleOperation(e))
-  );
-  
-  clearButton.addEventListener('click', e => {
-    init();
-  });
-  equalButton.addEventListener('click', () => {
-    num2 =
-      num2 || display.replace(num1, '').match(/[^\+|\-|\x|\/]\d*/);
-    result = operate(operatortion, num1, num2);
-    displayvalue.innerHTML = result % 1 ? Number(result.toFixed(4)) : result;
-    operation = '';
-  });
-  
-  init();
+
+const selectOperator = function (btn) {
+    if (bottomScreen.textContent === "0") return;
+    if (num1 === 0 || summed === true) {
+        operator = btn.target.textContent;
+        num1 = Number(bottomScreen.textContent);
+        num1.toFixed(2);
+        lastInput = [];
+        topScreen.textContent = `${checkIfFloat(num1)} ${operator}`;
+        summed = false;
+    }
+    else if (num1) {
+        chainOperators();
+        operator = btn.target.textContent;
+        topScreen.textContent = `${checkIfFloat(result)} ${operator}`;
+
+    }
+}
+
+
+const checkIfFloat = function (num) {
+    return Number.isInteger(num) ? Number(num) : Number(num.toFixed(2));
+};
+
+const clearData = function (btn) {
+    topScreen.textContent = "";
+    bottomScreen.textContent = "0";
+    operator = "";
+    num1 = 0;
+    num2 = 0;
+    lastInput = [];
+    result = 0;
+};
+
+
+
+
+const backspace = function () {
+    if (result) {
+        topScreen.textContent = "";
+        return
+    }
+    else {
+        lastInput.pop();
+        bottomScreen.textContent = lastInput.join("").toString();
+    }
+    if (lastInput.length === 0) {
+        bottomScreen.textContent = "0"
+    }
+}
+
+
+
+//event listeners
+const input = document.querySelectorAll(".number");
+input.forEach(button => button.addEventListener("click", display));
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(button => button.addEventListener("click", selectOperator));
+
+const equals = document.querySelector("#equal");
+equals.addEventListener("click", operate);
+
+const clearAllBtn = document.querySelector("#clear");
+clearAllBtn.addEventListener("click", clearData);
+
+const undoBtn = document.querySelector("#delete");
+undoBtn.addEventListener("click", backspace);
+
+
